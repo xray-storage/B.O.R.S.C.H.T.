ffe41e0d0d4b35e9d5b09933ad5d1bcfeec08163
abramcumner <abramcumner@yandex.ru>
2024-03-02 16:26:42 +0300

    608

--- version.h
+++ version.h
@@ -2,7 +2,7 @@
 #define BORSCHT_VERSION_H
 
 // Версия для компилятора ресурсов
-#define BORSCHT_RC_VERSION 1,8,10,582
-#define BORSCHT_APP_VERSION "1.8.10.582"
+#define BORSCHT_RC_VERSION 1,8,10,608
+#define BORSCHT_APP_VERSION "1.8.10.608"
 
 #endif
