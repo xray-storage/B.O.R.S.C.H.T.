a0a953f75f6ca3a4f554e642721f22c20930e5c5
abramcumner <abramcumner@yandex.ru>
2024-03-02 15:34:23 +0300

    ci: доработан сборочный скрипт

--- build.bat
+++ build.bat
@@ -1,3 +1,12 @@
-rem msbuild.exe engine.sln /p:Configuration=Release -t:Clean -target:xrgame -m
-rem msbuild.exe engine.sln /p:Configuration=Release -t:Clean -target:utils\xrai;utils\xrlc;utils\xrdo_light -m
-msbuild.exe engine.sln /p:Configuration=Release -t:Clean -target:editors\LevelEditor -m
+
+msbuild.exe engine.sln /p:Configuration=Release -t:Clean
+if errorlevel not 0 echo error: %errorlevel% && exit /b errorlevel
+
+msbuild.exe engine.sln /p:Configuration=Release -target:utils\xrai;utils\xrlc;utils\xrdo_light -m
+if %errorlevel% neq 0 echo error: %errorlevel% && exit /b errorlevel
+
+msbuild.exe engine.sln /p:Configuration=Release -target:editors\LevelEditor -m
+if %errorlevel% neq 0 echo error: %errorlevel% && exit /b errorlevel
+
+msbuild.exe engine.sln /p:Configuration=Release -target:xrgame -m
+if %errorlevel% neq 0 echo error: %errorlevel% && exit /b errorlevel
